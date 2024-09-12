f567cb279c8d31f8024620af35daa5a6868cd211
sergiotavuencas <sergiovicente_tavuenc@outlook.com>
2024-09-12 12:48:27 -0300

    Add initial: project dependencies and configurations.

---
+++ src/main/java/br/com/tavuencas/sergio/eventostec_api/EventostecApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tavuencas.sergio.eventostec_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventostecApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventostecApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tavuencas/sergio/eventostec_api/EventostecApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tavuencas.sergio.eventostec_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventostecApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
