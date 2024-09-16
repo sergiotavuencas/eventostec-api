d83e87ec0f29337af2523b793a0e4d77d0c129ff
sergiotavuencas <sergiovicente_tavuenc@outlook.com>
2024-09-16 11:52:18 -0300

    Fix: events table.
    Changed imgUrl to img_url.
    Changed eventUrl to event_url.
    Changed varchar from 100 to 150 for img_url due to insertion problem.

--- src/main/resources/db/migration/V1__create-table-events.sql
+++ src/main/resources/db/migration/V1__create-table-events.sql
@@ -4,8 +4,8 @@ CREATE TABLE events (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description VARCHAR(255),
-    imgUrl VARCHAR(100) NOT NULL,
-    eventUrl VARCHAR(100) NOT NULL,
+    img_url VARCHAR(150) NOT NULL,
+    event_url VARCHAR(100) NOT NULL,
     remote BOOLEAN NOT NULL,
     date TIMESTAMP NOT NULL
 );
