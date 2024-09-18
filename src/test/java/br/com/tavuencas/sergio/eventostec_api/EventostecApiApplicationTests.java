331a6b636228575de5eb0b2912d2cb151c448eb7
sergiotavuencas <sergiovicente_tavuenc@outlook.com>
2024-09-18 10:50:55 -0300

    Add test: dependency and application.
    Added the H2 database dependency for tests.
    Added application-test to use H2 database instead of postgres for tests.

--- src/test/java/br/com/tavuencas/sergio/eventostec_api/EventostecApiApplicationTests.java
+++ src/test/java/br/com/tavuencas/sergio/eventostec_api/EventostecApiApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.tavuencas.sergio.eventostec_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EventostecApiApplicationTests {
 
 	@Test
